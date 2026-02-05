80ebd54afcf61c1322bd9f6aa92cf46d00efef24
Kaden Berger <kadenb816@gmail.com>
2026-02-05 10:27:46 -0600

    adding neovim completion

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,34 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+  opts = function()
+    local cmp = require("cmp")
+
+    return {
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+      }),
+      experimental = {
+        ghost_text = true,
+      },
+    }
+  end,
+}
+
