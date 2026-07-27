f35ba5502c32983bb35b171485ba3bfae31c5e4b
Kaden Berger <kadenb816@gmail.com>
2026-07-26 19:24:04 -0500

    hyprland update animation fix

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -164,7 +164,7 @@ hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 
 -- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 500, dampening = 30 })
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
@@ -355,7 +355,7 @@ hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "m+1" }))
 hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "m-1" }))
 
 -- Open next available workspaceo on current monitor
-hl.bind(mainMod .. " + CTRL + mouse:273 ", hl.dsp.focus({ workspace = "emptym" }))
+hl.bind(mainMod .. " + SHIFT + mouse:273 ", hl.dsp.focus({ workspace = "emptym" }))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
